cce0d057689455569fb06f79cee767db1a33c88e
Phan Trần Huy <77103467+whoknow2001@users.noreply.github.com>
2022-07-28 18:32:15 +0700

    Update ITaskRepository.cs

--- Domain/Interfaces/Repositories/ITaskRepository.cs
+++ Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -5,7 +5,6 @@ namespace Domain.Interfaces.Repositories
     public interface ITaskRepository : IGenericRepository<Entities.Tasks.Task>
     {
         Task<Entities.Tasks.Task> GetOneByTodo(Guid todoId);
-
         Task<Entities.Tasks.Task> GetOneByAttachment(Guid attachmentId);
         Task<Entities.Tasks.Task?> GetTask(Guid taskId, Guid userId);
     }
